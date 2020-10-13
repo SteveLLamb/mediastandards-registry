1a5e83e026a1e9ea5e5bd176a2553bc564bbe935
Steve LLamb <steve.llamb@bydeluxe.com>
2020-10-12 17:31:36 -0700

    Added filtering search, project rename

--- src/site/features.js
+++ src/site/features.js
@@ -9,24 +9,325 @@ $(document).ready(function(){
   });
 });
 
-/* Clear filtering*/
+/* Clear filtering */
 
 $(document).on('click', '.clear-filter', function(){       
-    $("#searchTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf('') > -1)
-    });
-    document.getElementById("search-form").reset();
+
+  var table = $('#sorttable').DataTable();
+  table
+   .search( '' )
+   .columns().search( '' )
+   .draw();
+
+  $('#sorttable').DataTable().searchPanes.rebuildPane();
+  $('#sorttable').DataTable().order([0, 'asc']).draw();
+
 });
 
-/* DataTable options for sort headers*/
+/* DataTable options for sort headers and filtering */
 
 $(document).ready(function() {
-    $('#sorttable').DataTable( {
+
+    var dt = $('#sorttable').DataTable( {
       "paging": false,
-      "info": false,
-      "searching": false
+      searchPanes:{
+        cascadePanes: true,
+        emptyMessage:"<i><b>Empty</b></i>",
+        dtOpts: {
+          select: {
+              style: 'multi'
+          }
+        },  
+        layout: 'columns-4',
+        viewTotal: true,
+        columns: [1, 2, 3, 4]
+      },
+      dom: "<'row'<'col-sm-12'P>>" +
+          "<'row'<'col-sm-6'i><'col-sm-6'f>>" +
+          "<'row'<'col-sm-12't>>",
+      columnDefs:[
+        {
+          searchPanes: {
+            options:[
+              {
+                label: 'Accessibility',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Accessibility]');
+                }
+              },
+              {
+                label: 'Archive',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Archive]');
+                }
+              },
+              {
+                label: 'Audio',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Audio]');
+                }
+              },
+              {
+                label: 'Aux Data',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Aux Data]');
+                }
+              },
+              {
+                label: 'Captions',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Captions]');
+                }
+              },
+              {
+                label: 'Cinema Sound',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Cinema Sound]');
+                }
+              },
+              {
+                label: 'DCDM',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[DCDM]');
+                } 
+              },
+              {
+                label: 'DCinema',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[DCinema]');
+                }
+              },
+              {
+                label: 'DCP Application',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[DCP Application]');
+                }
+              },
+              {
+                label: 'DCP Core',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[DCP Core]');
+                }
+              },
+              {
+                label: 'DCinema',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[DCinema]');
+                }
+              },
+              {
+                label: 'Digital Source',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Digital Source]');
+                }
+              },
+              {
+                label: 'Distribution',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Distribution]');
+                }
+              },
+              {
+                label: 'Image',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Image]');
+                }
+              },
+              {
+                label: 'IMF',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[IMF]');
+                }
+              },
+              {
+                label: 'IMF Application',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[IMF Application]');
+                }
+              },
+              {
+                label: 'Immersive Audio',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Immersive Audio]');
+                }
+              },
+              {
+                label: 'Interop',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Interop]');
+                }
+              },
+              {
+                label: 'JPEG2000',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[JPEG2000]');
+                }
+              },
+              {
+                label: 'KDM',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[KDM]');
+                }
+              },
+              {
+                label: 'Measurement',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Measurement]');
+                }
+              },
+              {
+                label: 'Metadata',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Metadata]');
+                }
+              },
+              {
+                label: 'MXF Application',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[MXF Application]');
+                }
+              },
+              {
+                label: 'MXF Core',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[MXF Core]');
+                }
+              },
+              {
+                label: 'None Defined',
+                value: function(rowData, rowIdx){
+                    return rowData[3] == '';
+                }
+              },
+              {
+                label: 'Operations',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Operations]');
+                }
+              },
+              {
+                label: 'Packaging',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Packaging]');
+                }
+              },
+              {
+                label: 'Processing',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Processing]');
+                }
+              },
+              {
+                label: 'Projection',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Projection]');
+                }
+              },
+              {
+                label: 'Quality',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Quality]');
+                }
+              },
+              {
+                label: 'SDI',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[SDI]');
+                }
+              },
+              {
+                label: 'Security',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Security]');
+                }
+              },
+              {
+                label: 'Sign Language',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Sign Language]');
+                }
+              },
+              {
+                label: 'Sound',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Sound]');
+                }
+              },
+              {
+                label: 'Subtitles',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[Subtitles]');
+                }
+              },
+              {
+                label: 'XML',
+                value: function(rowData, rowIdx){
+                    return rowData[3].includes('[XML]');
+                }
+              }
+            ]
+          },
+          targets:[3],
+        },
+        {
+          searchPanes: {
+            options:[
+              {
+                label: 'Active',
+                value: function(rowData, rowIdx){
+                  return rowData[4].includes('[ Active ]');
+                }
+
+              },
+              {
+                label: 'Amended',
+                value: function(rowData, rowIdx){
+                  return rowData[4].includes('[ Amended ]');
+                }
+
+              },
+              {
+                label: 'Draft',
+                value: function(rowData, rowIdx){
+                  return rowData[4].includes('[ Draft ]');
+                }
+
+              },
+              {
+                label: 'Superseded',
+                value: function(rowData, rowIdx){
+                  return rowData[4].includes('[ Superseded ]');
+                }
+
+              },
+              {
+                label: 'Unknown',
+                value: function(rowData, rowIdx){
+                  return rowData[4].includes('[ Unknown ]');
+                }
+
+              },
+              {
+                label: 'Withdrawn',
+                value: function(rowData, rowIdx){
+                  return rowData[4].includes('[ Withdrawn ]');
+                }
+
+              }
+            ]
+          },
+          targets: [4]
+        },
+      ]
     });
-} );
+    dt.on('select.dt', () => {          
+      dt.searchPanes.rebuildPane(0, true);
+    });
+ 
+    dt.on('deselect.dt', () => {
+      dt.searchPanes.rebuildPane(0, true);
+    });
+});
 
 /* "Back To Top" button functionality */
 
